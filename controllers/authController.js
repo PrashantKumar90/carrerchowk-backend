4055d6bd13fed6f7edbbaef149d8fa731cb4b025
PrashantKumar90 <prashantkumarstm2004@gmail.com>
2025-07-13 12:32:06 +0530

    Passcode update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,10 @@
 const jwt = require("jsonwebtoken");
 
-// Hardcoded credentials
-const HARD_CODED_USER = {
-  email: "prashant17924@gmail.com",
-  password: "*PRASANT@salo!821119#",
-};
-
-// Login User
 const loginUser = (req, res) => {
   const { email, password } = req.body;
 
 
-  if (email === HARD_CODED_USER.email && password === HARD_CODED_USER.password) {
+  if (email === process.env.USERNAME && password === HARD_CODED_USER.password) {
     const token = jwt.sign(
       { userId: "1", email: HARD_CODED_USER.email },
       process.env.JWT_SECRET,
